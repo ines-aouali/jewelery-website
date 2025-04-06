5b5f49c2d361748a65144026e9e6a3bee62e6da2
Ines Aouali <ines.aouali17@gmail.com>
2025-04-06 12:13:46 +0100

    login/signup part

---
+++ script.js
@@ -0,0 +1,43 @@
+const formOpenBtn = document.querySelector("#form-open"),
+  home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+
+// Open login form when clicking the login button
+console.log("JavaScript is running!");
+formOpenBtn.addEventListener("click", () => {
+  console.log("Login button clicked!");
+  home.classList.add("show");
+});
+formCloseBtn.addEventListener("click", () => {
+  console.log("Close button clicked!");
+  home.classList.remove("show");
+});
+
+// Toggle password visibility
+// pwShowHide.forEach((icon) => {
+//   icon.addEventListener("click", () => {
+//     let getPwInput = icon.previousElementSibling; // Get the input field before the icon
+//     if (getPwInput.type === "password") {
+//       getPwInput.type = "text";
+//       icon.classList.replace("fa-eye-slash", "fa-eye"); // Change to eye
+//     } else {
+//       getPwInput.type = "password";
+//       icon.classList.replace("fa-eye", "fa-eye-slash"); // Change back to eye-slash
+//     }
+//   });
+// });
+
+// Toggle between login and signup forms
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
